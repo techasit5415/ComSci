c17875c82f8697f703b23b3e6b5a94208f434104
Your Name <techasit2424@gmail.com>
2024-03-25 12:29:29 +0700

    edit lab9_1

---
+++ Y.1/Y.1.2/final oop/lab10/lab10_1.java
@@ -0,0 +1,17 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+public class lab10_1 {
+    public static void main(String[] args) {
+        example1();
+    }
+    private static void example1(){
+        List<Integer> number = Arrays.asList(4, 5, 6, 7);
+        number.forEach(n -> System.out.print(n));
+    }
+    private static void example2(){
+        
+    }
+
+}
--- Y.1/Y.1.2/final oop/lab9/lab9_1.java
+++ Y.1/Y.1.2/final oop/lab9/lab9_1.java
@@ -2,9 +2,20 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
-public class lab9_1{
-//generic
+public class lab9_1 {
+    // generic
     public static void main(String[] args) {
+        example1();
+        example2();
+        example3();
+        example4();
+        example5();
+        example6();
+        example7();
+
+    }
+
+    private static void example1() {
         printer<Integer> number = new printer<>();
         number.sett(1);
         number.print();
@@ -17,24 +28,32 @@ public class lab9_1{
         STR.sett("set str");
         STR.print();
 
+    }
+
+    private static void example2() {
         System.out.println();
         System.out.println("------example2--------");
         System.out.println();
 
-        printer1 exam =new printer1();
-        System.out.println(":return from printer1 :"+ exam.example2("john", 77));
-        
+        printer1 exam = new printer1();
+        System.out.println(":return from printer1 :" + exam.example2("john", 77));
+    }
+
+    private static void example3() {
+        printer1 exam = new printer1();
         System.out.println();
         System.out.println("------example3--------");
         System.out.println();
         List<Integer> numList = new ArrayList<>();
         numList.add(3);
         exam.example3(numList);
-        
+
         List<String> strList = new ArrayList<>();
         strList.add("Test");
         exam.example3(strList);
+    }
 
+    private static void example4() {
         System.out.println("------example4--------");
         HashSet<String> cars = new HashSet<>();
         cars.add("Volvo");
@@ -43,7 +62,9 @@ public class lab9_1{
         cars.add("BMW");
         cars.add("Mazda");
         System.out.println(cars);
-      
+    }
+
+    private static void example5() {
         System.out.println("------example5--------");
         HashSet<Integer> numcars = new HashSet<>();
         numcars.add(1);
@@ -52,21 +73,27 @@ public class lab9_1{
         numcars.add(6);
         numcars.add(9);
         System.out.println(numcars);
-        System.out.println("------example5--------");
+    }
 
+    private static void example6() {
+        System.out.println("------example6--------");
+        HashSet<String> cars = new HashSet<>();
+        System.out.println("Mazda");
         if (cars.contains("Mazda")) {
             System.out.println("str 'Mazda'");
         } else {
             System.out.println("str no have 'Mazda'");
         }
+    }
 
-        System.out.println("------example6--------");
-        
+    private static void example7() {
+        System.out.println("------example7--------");
+        HashSet<String> cars = new HashSet<>();
+        System.out.println("mazda");
         if (cars.contains("mazda")) {
             System.out.println("str have'mazda'");
         } else {
             System.out.println("str no have 'mazda'");
         }
     }
-    
 }
